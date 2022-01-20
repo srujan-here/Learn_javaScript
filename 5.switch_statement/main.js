e16ca1071d68c402733fda1656512e78b187a092
Putta Srujan <93040995+srujan-here@users.noreply.github.com>
2022-01-20 14:04:04 +0530

    Switch Statement in javascript

---
+++ 5.switch_statement/main.js
@@ -0,0 +1,14 @@
+var t=prompt("enter the name of the fruit");
+switch(t){
+    case "Apple":
+        console.log("Apples are 200 rupees per kg");
+        break;
+    case "Mango":
+    console.log("Mangoes are 100 rupees per kg");
+    break;
+    case "Banana":
+    console.log("Dozen bananas are for rupess 50");
+    break;
+    default:
+    console.log(`${t} is not available at present`);
+}
