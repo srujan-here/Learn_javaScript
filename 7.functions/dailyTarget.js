540dff04b54d0dc4207bfddf157bd65baa408299
Putta Srujan <93040995+srujan-here@users.noreply.github.com>
2022-01-20 21:58:30 +0530

    functions in javascript

---
+++ 7.functions/dailyTarget.js
@@ -0,0 +1,7 @@
+function run(hours,min,sec){
+    console.log("you should spend "+hours+" hours "+min+" minutes "+sec+" seconds daily in learning javascript");
+}
+h=prompt('enter number of hours');
+m=prompt('enter number of minutes');
+s=prompt('enter number of seconds');
+run(h,m,s);
---
+++ 7.functions/setTarget.js
@@ -0,0 +1,6 @@
+function learnjs(days,hours,minutes){
+    console.log("target fixed for "+days+" days "+hours+" hours "+minutes+" minutes"+" to complete javascript full course")
+}
+console.log("set target to complete javascript course");
+learnjs(20,10,5);
+
