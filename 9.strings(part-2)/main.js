0a455d6ab7599ce2a0051ac483d91a61c9ebfcd1
Putta Srujan <93040995+srujan-here@users.noreply.github.com>
2022-01-23 17:05:52 +0530

    String functions in javascript

---
+++ 9.strings(part-2)/main.js
@@ -0,0 +1,17 @@
+let t="srujan is a programmer"
+console.log(t.length)
+
+//last character
+console.log(t[t.length-1])
+
+//finding the index
+console.log(t.indexOf("is"))
+
+//finding the substring
+console.log(t.slice(5,14))
+
+//upper case
+var r="sRuJan"
+console.log(r.toUpperCase())
+//lower case
+console.log(r.toLowerCase())
