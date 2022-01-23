91a2404afb29c1582e0c6d670b09372780eb7084
Putta Srujan <93040995+srujan-here@users.noreply.github.com>
2022-01-23 16:17:00 +0530

    Intro of strings in javascript

---
+++ 8.strings(part-1)/main.js
@@ -0,0 +1,15 @@
+let s="srujan"
+let t=s+" hello"
+console.log(t);
+let m1="35"+4;
+console.log(m1)
+
+//String to number
+let num="100"
+let n1=Number(num)
+console.log(typeof(n1))
+
+//Number to string
+let s1=100;
+let s2=Number.toString(s1);
+console.log(typeof(s2))
