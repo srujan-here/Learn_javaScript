e7fd13e949bb15e6d28fa25a4d350aa8f34bf07d
Putta Srujan <93040995+srujan-here@users.noreply.github.com>
2022-01-26 13:21:40 +0530

    Coercion and Type Conversion in javascript

---
+++ 11.Coercion and Type Conversion/main.js
@@ -0,0 +1,19 @@
+let t=10
+let m="10"+t
+console.log(m)  //converts to string
+
+let num="10"
+let num2=10
+console.log(num2/num)  //converts to number
+
+
+console.log(true+false)  //true-1, false-0
+
+console.log(15+10+"123")   //compiler adds up 1st two numbers
+
+console.log("true"==true)   //due to == operator compiler converts string to number
+
+
+
+//Refer :https://dorey.github.io/JavaScript-Equality-Table/
+
